12625e4d6861e814b075fc73665662e745a68f96
Rehan Shah <flyrehan786@gmail.com>
2024-09-30 15:09:31 +0500

    adding some components.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { UsersComponent } from './components/auth/users/users.component';
+import { ItemsComponent } from './components/pages/items/items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    UsersComponent,
+    ItemsComponent
   ],
   imports: [
     BrowserModule,
